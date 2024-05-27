7f392815d461649c584d6989096e8ff1e4f998ed
Никита <153456515+Lairont@users.noreply.github.com>
2024-05-27 14:30:59 +0300

    Add files via upload

---
+++ давно1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace давно1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task_1
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={x + y}");
+            //Console.ReadLine();
+            //Task_2
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.Write("z=");
+            //int z = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}+{z}={x + y + z}");
+            //Console.ReadLine();
+            //Task_3
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x:f1}*{y:f1}={x * y:f1}");
+            //Console.ReadLine();
+            //Task_4
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x:f3}/{y:f3}={x + y:f3}");
+            //Console.ReadLine();
+            //Task_5
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a}+{b}={b}+{a}");
+            //Console.ReadLine();
+            //Task_6
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"a+b={a}+{b}={b + a}\ta-b={a}-{b}={a-b}\ta*b={a}*{b}={a*b}");
+            //Console.ReadLine();
+            //Task_7
+            //Console.Write("Как тебя зовут?  ");
+            // string a = (Console.ReadLine());
+            //Console.Write("Сколько тебе лет?  ");
+            // int b = int.Parse(Console.ReadLine());
+            //int i = 2009 - b;
+            //Console.Write($"{a}, ты родился(ась) в {i} году");
+            //Console.ReadLine();
+            //Task_8
+            //Console.Write("Наминал купюр = ");
+            //int a =  int.Parse(Console.ReadLine());
+            //Console.Write("Колличество купюр = ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write($"Сумма денег= {a*b:f2}");
+            //Console.ReadLine();
+            //Task_9
+            //Console.Write("Сторона квадрата равна - ");
+            //int a = int.Parse(Console.ReadLine());
+            //int P = 4 * a;
+            //Console.Write($"P={P}");
+            //Console.ReadLine();
+        }
+    }
+}
